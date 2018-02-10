6a1c36cb6c3b9b71b5a41b810eb8b9df8a9b2d9d
carlos calla <carlos.calla89@gmail.com>
2018-02-10 13:20:52 -0500

    Express Rest

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes',(req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('content-type','text/plain');
+    next();
+});
+
+
+app.get('/dishes', (req,res,next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes',(req,res,next) => {
+    res.statusCode = 403;
+    res.end('Post operation not supported on /dishes/');
+});
+
+app.put('/dishes/:dishId',(req,res,next) => {
+    res.write('Updating the dish: '+req.params.dishId);
+    res.end('Will update the dish: '+req.body.name + ' with details: '+
+    req.body.description);
+});
+
+app.delete('/dishes',(req,res,next) => {
+    res.end('Delete dish: '+req.params.description);
+})
+
+app.get('/dishes/:dishId',(req,res,next) => {
+    res.end('Will send details of the dish: '+
+    req.params.dishId +' to you');
+})
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+  });
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + 
+          ' with details: ' + req.body.description);
+});
+  
+app.delete('/dishes/:dishId', (req, res, next) => {
+      res.end('Deleting dish: ' + req.params.dishId);
+});
+
+
+
+
 
 app.use(express.static(__dirname+'/public'));
 
@@ -14,7 +66,7 @@ app.use((req,res,next) => {
     console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('content-type','text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+    res.end('<html><body><h1>This is an Express Server Test</h1></body></html>');
 });
 
 const server = http.createServer(app);
